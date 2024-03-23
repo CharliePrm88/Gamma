f288a96de52410b14ce7a6cad3a4b8b426fc78cc
Stefano Ponzi <iznopste@gmail.com>
2024-03-23 09:19:17 +0100

    Fixed rest-urls; Fixed some wrong mapping

--- backend/gamma-common/src/main/java/it/sponzi/gamma/common/controller/BaseController.java
+++ backend/gamma-common/src/main/java/it/sponzi/gamma/common/controller/BaseController.java
@@ -6,7 +6,10 @@ import it.sponzi.gamma.common.service.BaseService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -20,37 +23,37 @@ public class BaseController<S extends BaseService<T, M>, T extends BaseDao, M ex
 
     @Override
     @GetMapping(value = Urls.FIND_BY_ID, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<M> findById(Long id) {
+    public Mono<M> findById(@PathVariable Long id) {
         return service.findById(id);
     }
 
     @Override
-    @GetMapping(value = Urls.FIND_BY_ID, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = Urls.FIND_ALL, produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<M> findAll() {
         return service.findAll();
     }
 
     @Override
-    @GetMapping(value = Urls.FIND_BY_ID, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Flux<M> findAll(String search) {
+    @GetMapping(value = Urls.FIND_ALL + "{search}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<M> findAll(@PathVariable String search) {
         return service.findAll(search);
     }
 
     @Override
-    @PostMapping(value = Urls.FIND_BY_ID, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<M> create(M dtoToBeSaved) {
+    @PostMapping(value = Urls.CREATE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<M> create(@RequestBody M dtoToBeSaved) {
         return service.create(dtoToBeSaved);
     }
 
     @Override
-    @PostMapping(value = Urls.FIND_BY_ID, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<M> update(M dtoToBeSaved) {
+    @PutMapping(value = Urls.UPDATE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<M> update(@RequestBody M dtoToBeSaved) {
         return service.update(dtoToBeSaved);
     }
 
     @Override
-    @DeleteMapping(value = Urls.FIND_BY_ID, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<Void> deleteById(Long id) {
+    @DeleteMapping(value = Urls.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Void> deleteById(@PathVariable Long id) {
         return service.deleteById(id);
     }
 }
